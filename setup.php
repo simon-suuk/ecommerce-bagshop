5cc30d435c90f3ee54794ba46a199516d37bf53c
ephraimayite <ephraimayite@gmail.com>
2017-01-26 13:47:07 +0000

    made changes for customers

--- setup.php
+++ setup.php
@@ -10,31 +10,20 @@
 <?php // Example 26-3: setup.php
   require_once 'functions.php';
 
-  createTable('members',
-              'user VARCHAR(16),
-              pass VARCHAR(16),
-              INDEX(user(6))');
-
-  createTable('messages', 
-              'id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-              auth VARCHAR(16),
-              recip VARCHAR(16),
-              pm CHAR(1),
-              time INT UNSIGNED,
-              message VARCHAR(4096),
-              INDEX(auth(6)),
-              INDEX(recip(6))');
-
-  createTable('friends',
-              'user VARCHAR(16),
-              friend VARCHAR(16),
-              INDEX(user(6)),
-              INDEX(friend(6))');
-
-  createTable('profiles',
-              'user VARCHAR(16),
-              text VARCHAR(4096),
-              INDEX(user(6))');
+  createTable('customers',
+              'cno BIGINT(5),
+              cname VARCHAR(30),
+              street VARCHAR(30).
+			  zip DECIMAL(5,0),
+			  phone CHAR(12)
+			  )');
+			  
+		
+	insertCustomer("Ephraim Ayite",
+			"Boulervard du Zio",
+			332,
+			"002335467665");
+  
 ?>
 
     <br>...done.
